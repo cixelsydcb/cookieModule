ed8c3d467797224b6c464a27619f29c17749b366
Charles Keith Brown <charles.k.brown@gmail.com>
2016-09-17 15:56:31 +1000

    Various Fixes

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/main.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/main.py
@@ -4,7 +4,8 @@
 from {{cookiecutter.repo_name}} import {{cookiecutter.app_class_name}}
 
 def main():
-    {{cookiecutter.app_class_name}}().run()
+    res = {{cookiecutter.app_class_name}}().run()
+    print(res)
 
 if __name__ == '__main__':
     main()
--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}.py
@@ -8,7 +8,8 @@
 
 class {{cookiecutter.app_class_name}}:
     """Basic class"""
-    def __init__():
+    def __init__(self):
         self.hello = "Hello, World"
-    def run():
-        print(self.hello)
+    
+    def run(self):
+        return self.hello
